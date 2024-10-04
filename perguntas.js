babdaeb75432479604017f7b2be5c6da8c8838ed
pereira-99 <133021383+pereira-99@users.noreply.github.com>
2024-10-04 10:39:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'marca de moto',
+    'qual a marca da titan 150?',
+    'honda'
+)
+
+criaCartao(
+    'carro',
+    'Qual o carro mais rapido do mundo?',
+    'o carro mais rapido do mundo, é o jesko Absolut, chegando a incriveis 531 mk/h'
+)
+
+criaCartao(
+    'carro',
+    'qual o carro mais caro do mundo?',
+    'o carro mais caro do mundo se chama Rolls-Royce Boat tail, custando a bagatela de R$ 139 milhões'
+)
+
+criaCartao(
+    'carros',
+    'qual a garagem mais cara do Brasil',
+    'a garagem mais cara do Brasil é do junior, tendo a unica Bugatti chiron Sport do brasil, a la ferrari, Mclaren p1 e muito mais'
+) 
+
+criaCartao(
+    ' paul walker ',
+    'em qual filme da franquia velozes e furiosos Paul Walker faleceu( na vida real)',
+    'Paul Walker faleu , enquanto o filme velozes e furiosos 7 estava sendo feito'
+) 
+
+criaCartao(
+    ' corinthinas ',
+    'quem é o artilheiro da neo quimica arena( corinthians)',
+    'Angel romero, o paraguay da torcida que canta e grita'
+) 
+
+criaCartao(
+    ' corinthinas ',
+    'quem foi o capitão do corinthians na final do mundial de 2012',
+    'Alessandro Nunes, o capitão do corinthinas na conquista do segundo mundial de clubes da fifa'
+) 
+
+criaCartao(
+    ' corinthinas ',
+    'quem foi o goleiro titulor do corinthians no mundial de 2012',
+    'Cassio ramos, o goleiro q consagrou o corinthinas campeão em 90%, com grandes defesas durante a partida'
+) 
+
+criaCartao(
+    ' corinthinas ',
+    'quem fez o gol do titulo do corinthinas no mundial de 2012',
+    'Paolo Guerreiro, fez o gol de cabeça na pequena area, num rebote do golerio piter cech'
+) 
